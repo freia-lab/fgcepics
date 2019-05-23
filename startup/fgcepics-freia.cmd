229528e9d202c9d775615acdfeae2d2065187a86
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-05-23 17:45:31 +0200

    Set the proper PV name prefix. Added some documentation.

--- startup/fgcepics-freia.cmd
+++ startup/fgcepics-freia.cmd
@@ -41,8 +41,8 @@ drvAsynIPPortConfigure ("fgc_port_1" ,"localhost:1906", 0, 0, 0)
 #  3) timeout        #
 #--------------------#
 
-devFgcUdpConfig("fgc_udp", 2905)
-devFgcUdpRegisterHost("fgc_udp", "pcte23185", "0x00fe", 4.0)
+devFgcUdpConfig("fgc_udp", 2906)
+devFgcUdpRegisterHost("fgc_udp", "cbox1-0", "0x00ff", 5.0)
 
 
 ######## Configure UDP published data driver ########
@@ -58,7 +58,7 @@ devFgcUdpRegisterHost("fgc_udp", "pcte23185", "0x00fe", 4.0)
 #  3) fgc_class                                                              #
 #----------------------------------------------------------------------------#
 
-devFgcUdpRegisterDev("pcte23185", 1, 63, "FGC_1")
+devFgcUdpRegisterDev("cbox1-0", 2, 63, "FGC.2")
 #devFgcUdpRegisterDev("pcte23185", 4, 0,  "FGC_4")
 
 
@@ -72,13 +72,13 @@ devFgcUdpRegisterDev("pcte23185", 1, 63, "FGC_1")
 
 ######## Load record instances - FGC udp ########
 
-dbLoadRecords("fgcudp_class_63.db",  "PORT=fgc_udp,HOST=,DEV=FGC_1:,FGC=FGC_1")
+dbLoadRecords("fgcudp_class_63.db",  "PORT=fgc_udp,HOST=MAG-,DEV=FGC1:,FGC=FGC.2")
 #dbLoadRecords("fgcudp_class_base.db","PORT=fgc_udp,HOST=,DEV=FGC_4:,FGC=FGC_4")
 
 
 ######## Load record instances - FGc cmd ########
 
-dbLoadRecords("fgccmd_class_63.db",  "PORT=fgc_port_1,HOST=,DEV=FGC_1:,FGC=FGC_1")
+dbLoadRecords("fgccmd_class_63.db",  "PORT=fgc_port_1,HOST=MAG-,DEV=FGC1:,FGC=FGC.2")
 #dbLoadRecords("fgccmd_class_63.db",  "PORT=fgc_port_2,HOST=,DEV=FGC_2:,FGC=2")
 #dbLoadRecords("fgccmd_class_63.db",  "PORT=fgc_port_3,HOST=,DEV=FGC_3:,FGC=3")
 #dbLoadRecords("fgccmd_class_63.db",  "PORT=fgc_port_4,HOST=,DEV=FGC_4:,FGC=4")
