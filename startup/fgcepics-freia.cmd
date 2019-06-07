21ac0ce604554bc65c962afd0cca22dff7762bea
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2019-06-07 23:32:46 +0200

    Added commands from the example script.

--- startup/fgcepics-freia.cmd
+++ startup/fgcepics-freia.cmd
@@ -26,8 +26,23 @@ epicsEnvSet("PV_PREFIX", "MAG-FGC")
 #  - Recomend using the 'drvAsynIPPortConfigure' driver.      #
 #-------------------------------------------------------------#
 
+# Example:
+#
+# - 2 hosts / 4 devices
+#   * Host #0:
+#     ** FGC #1 : cmd_port_0_1
+#   * Host #1:
+#     ** FGC #1 : cmd_port_1_1
+#     ** FGC #2 : cmd_port_1_2
+#     ** FGC #3 : cmd_port_1_3
+
+#drvAsynIPPortConfigure("cmd_port_0_1" ,"host_0_addr:1906", 0, 0, 0)
+#drvAsynIPPortConfigure("cmd_port_1_1" ,"host_1_addr:1906", 0, 0, 0)
+#drvAsynIPPortConfigure("cmd_port_1_2" ,"host_1_addr:1906", 0, 0, 0)
+#drvAsynIPPortConfigure("cmd_port_1_3" ,"host_1_addr:1906", 0, 0, 0)
+
 drvAsynIPPortConfigure ("fgc_port_1" ,"localhost:1906", 0, 0, 0)
-#drvAsynIPPortConfigure ("fgc_port_2" ,"localhost:1906", 0, 0, 0)
+drvAsynIPPortConfigure ("fgc_port_2" ,"localhost:1906", 0, 0, 0)
 #drvAsynIPPortConfigure ("fgc_port_3" ,"localhost:1906", 0, 0, 0)
 #drvAsynIPPortConfigure ("fgc_port_4" ,"localhost:1906", 0, 0, 0)
 
@@ -35,29 +50,89 @@ drvAsynIPPortConfigure ("fgc_port_1" ,"localhost:1906", 0, 0, 0)
 
 ######## Start UDP published data driver ########
 
-#--------------------#
-# Parameters:        #
-#  1) port_name      #
-#  2) local_udp_port #
-#  3) timeout        #
-#--------------------#
+#--------------------------------------------------------#
+# Parameters:                                            #
+#  1) Asyn port name                                     #
+#  2) UDP IP port                                        #
+#  3) Timeout                                            #
+#                                                        #
+# NOTE: Default UDP port values may have to be modified. #
+#       Different gateways can use different UDP ports.  #
+#--------------------------------------------------------#       
+
+# Example:
+#
+# - 2 hosts, sending to two different UDP ports.
+# - Alternatively, they could use the same UDP port.
+#   * Host #0:
+#     ** udp_port_0, ip-port 2906
+#   * Host #1:
+#     ** udp_port_1, ip-port 2905
+
+#devFgcUdpConfig("udp_port_0" , 2906)
+#devFgcUdpConfig("udp_port_1" , 2905)
 
 devFgcUdpConfig("fgc_udp", 2906)
+
+######## Configure published data driver - hosts ########
+
+#----------------------------------------------------------------------------#
+# Notes:                                                                     #
+#  - Call 1 per each Host.                                                   #
+#                                                                            #
+# Parameters:                                                                #
+#  1) Asyn port name                                                         #
+#  2) Host name                                                              #
+#  3) Host publication ID                                                    #
+#  4) Timeout                                                                #
+#----------------------------------------------------------------------------#
+
+# Example:
+#
+# - 2 hosts, with different names and IDs:
+#   * Host #0:
+#     ** EPICS port: udp_port_0
+#     ** Host name:  name_host_0
+#     ** ID:         0x00ff
+#     ** Timeout:    5.0
+#   * Host #1:
+#     ** EPICS port: udp_port_1
+#     ** Host name:  name_host_1
+#     ** ID:         0x00fe
+#     ** Timeout:    10.0
+
+#devFgcUdpRegisterHost("udp_port_0" , "name_host_0", "0x00ff", 5.0)
+#devFgcUdpRegisterHost("udp_port_1" , "name_host_1", "0x00fe", 10.0)
+
 devFgcUdpRegisterHost("fgc_udp", "cbox1-0", "0x00ff", 5.0)
 
-
-######## Configure UDP published data driver ########
+######## Configure published data driver - devices ########
 
 #----------------------------------------------------------------------------#
 # Notes:                                                                     #
 #  - Call 1 per each FGC.                                                    #
-#  - Class #0 is a placeholder for the properties that common to all classes.#
 #                                                                            #
 # Parameters:                                                                #
-#  1) port_name                                                              #
-#  2) fgc_id                                                                 #
-#  3) fgc_class                                                              #
+#  1) Host name                                                              #
+#  2) FGC ID (dongle)                                                        #
+#  3) FGC class                                                              #
+#  4) FGC name (name file notation)                                          #
 #----------------------------------------------------------------------------#
+
+# Example:
+#
+# - 2 hosts / 4 devices
+#   * Host #0:
+#     ** FGC #1 : dev.name.0
+#   * Host #1:
+#     ** FGC #1 : dev.name.1
+#     ** FGC #2 : dev.name.2
+#     ** FGC #3 : dev.name.3
+
+#devFgcUdpRegisterDev("host_name_0" , 1, 63, "dev.name.0")
+#devFgcUdpRegisterDev("host_name_1" , 1, 63, "dev.name.1")
+#devFgcUdpRegisterDev("host_name_1" , 2, 63, "dev.name.2")
+#devFgcUdpRegisterDev("host_name_1" , 3, 63, "dev.name.3")
 
 devFgcUdpRegisterDev("cbox1-0", 2, 63, "FGC.2")
 #devFgcUdpRegisterDev("pcte23185", 4, 0,  "FGC_4")
@@ -74,6 +149,7 @@ devFgcUdpRegisterDev("cbox1-0", 2, 63, "FGC.2")
 ######## Load record instances - FGC udp ########
 
 dbLoadRecords("fgcudp_class_63.db",  "PORT=fgc_udp,HOST=MAG-,DEV=FGC1:,FGC=FGC.2")
+dbLoadRecords("fgcudp_class_63.db",  "PORT=fgc_udp,HOST=MAG-,DEV=FGC2:,FGC=FGC.3")
 #dbLoadRecords("fgcudp_class_base.db","PORT=fgc_udp,HOST=,DEV=FGC_4:,FGC=FGC_4")
 
 
